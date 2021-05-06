0bfd9be6b802206def288caf2338d7ffbc1ff388
maddenlockin <maddenlockin@gmail.com>
2021-05-06 10:20:09 -0700

    image update and win loss display works

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // import functions and grab DOM elements
-import {  } from './utils.js';
+import { didUserWin } from './utils.js';
+import { getRandomThrow } from './get-random-throw.js';
 // initialize state
 const shoot = document.querySelector('#shoot-button');
 const feedbackDiv = document.querySelector('#feedback-div');
 const winTotalDiv = document.querySelector('#total-wins');
 const lossTotalDiv = document.querySelector('#total-losses');
 
-console.log(shoot, feedbackDiv, winTotal, lossTotal);
+console.log(shoot, feedbackDiv, winTotalDiv, lossTotalDiv);
 
 let winTotal = 0;
 let lossTotal = 0;
@@ -14,8 +15,24 @@ let lossTotal = 0;
 shoot.addEventListener('click', () => {
     const selectedRadio = document.querySelector('input:checked');
     const userThrow = selectedRadio.value;
-    let autoThrow = Math.ceil(Math.random() * 3);
 
+    let autoThrow = getRandomThrow();
+
+    const result = didUserWin(userThrow, autoThrow);
+    // set a variable equal to the string retured from did user win, passing in userthrow and autothrow
+
+    if (result === 'win') {
+        winTotal++;
+        winTotalDiv.textContent = winTotal;
+    }
+    if (result === 'lose') {
+        lossTotal++;
+        lossTotalDiv.textContent = lossTotal;
+    }
+    if (result === 'draw') {
+        ;
+    }
+    //when added to html, can put the variable result straight into my html (.textcontent), + to add "you" (a sting and a var)
 });
   // get user input
   // use user input to update state 
--- utils.js
+++ utils.js
@@ -1,16 +1,16 @@
-import { getRandomThrow } from './get-random-throw.js';
-
 export function didUserWin(userThrow, compThrow) {
         // rock > scissors
         //scissors > paper
         //paper > rock
     //const compThrow = getRandomThrow();
     if (compThrow === userThrow) return 'draw';
+
     else if ((compThrow === 'rock' && userThrow === 'scissors') 
     || (compThrow === 'scissors' && userThrow === 'paper') 
     || (compThrow === 'paper' && userThrow === 'rock')) {
         return 'lose';
     }
+    
     else return 'win';
 
 
