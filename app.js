416e11b62827b7134f5f8649a08f59e7959c82d8
maddenlockin <maddenlockin@gmail.com>
2021-05-05 17:19:02 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // import functions and grab DOM elements
-
+import {  } from './utils.js';
 // initialize state
+const shoot = document.querySelector('#shoot-button');
+const feedbackDiv = document.querySelector('#feedback-div');
+const winTotalDiv = document.querySelector('#total-wins');
+const lossTotalDiv = document.querySelector('#total-losses');
 
+console.log(shoot, feedbackDiv, winTotal, lossTotal);
+
+let winTotal = 0;
+let lossTotal = 0;
 // set event listeners 
+shoot.addEventListener('click', () => {
+
+
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
