8d059b2ac8f179e3e20b58c9b898f25fc2f81e6c
Alonzo A <68953196+Alonzo-Anderson@users.noreply.github.com>
2021-05-07 15:01:28 -0700

    refactorrrrr

--- get-random-throw.js
+++
@@ -1,6 +0,0 @@
-export function getRandomThrow() {
-    let autoThrow = Math.ceil(Math.random() * 3);
-    if (autoThrow === 1) return 'rock';
-    else if (autoThrow === 2) return 'scissors';
-    else return 'paper';
-}
--- utils.js
+++ utils.js
@@ -3,15 +3,19 @@ export function didUserWin(userThrow, compThrow) {
         //scissors > paper
         //paper > rock
     //const compThrow = getRandomThrow();
-    if (compThrow === userThrow) return 'draw';
-
+    if (compThrow === userThrow) 
+    {return 'draw';}
     else if ((compThrow === 'rock' && userThrow === 'scissors') 
     || (compThrow === 'scissors' && userThrow === 'paper') 
     || (compThrow === 'paper' && userThrow === 'rock')) {
         return 'lose';
     }
-    
     else return 'win';
-
-
+}
+//get comp throw
+export function getRandomThrow() {
+    let autoThrow = Math.ceil(Math.random() * 3);
+    if (autoThrow === 1) return 'rock';
+    else if (autoThrow === 2) return 'scissors';
+    else return 'paper';
 }
